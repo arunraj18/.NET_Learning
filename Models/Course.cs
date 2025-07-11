99eb6abfe3026429f492149c357a0c1ef5e7da2a
pbonala1 <pravalikabonala@gmail.com>
2025-07-11 14:43:14 -0500

    Models created

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FirstApp.Models
+{
+	public class Course
+	{
+		private int CourseId { get; set; }
+
+		[Required]
+        [StringLength(60)]
+		private string CourseName { get; set; }
+	}
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace FirstApp.Models
+{
+	public class Student
+	{
+		
+			private int Id { get; set; }
+
+            [Required]
+            [StringLength(30)]
+            private string Name { get; set; }
+
+            [EmailAddress]
+            private string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            private DateTime EnrollmentDate { get; set; }
+
+
+    }
+}
+
