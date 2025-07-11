1eaa1a6d36dfe5e56f0832aeed8a7328ae64f43d
apeddhala <arunraj.peddhala@dxc.com>
2025-07-11 17:06:34 -0500

    added put and delete endpoints

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,51 @@
+﻿using FirstApp.Models;
+using FirstApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        [HttpPut]
+        public IActionResult UpdateStudents([FromBody] Student student) {
+            if (student == null || student.Id <= 0)
+            {
+                return BadRequest();
+            }
+            var currStudent = StudentRepository.students.Where(s => s.Id == student.Id).FirstOrDefault();
+            StudentRepository.students.Remove(currStudent);
+            if (currStudent == null)
+            {
+                return NotFound();
+            }
+            currStudent.Id = student.Id;
+            currStudent.Name = student.Name;
+            currStudent.Email = student.Email;
+            currStudent.EnrollmentDate = student.EnrollmentDate;
+            StudentRepository.students.Add(currStudent);
+            return Ok();
+        
+        
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult DeleteStudent(int studentId)
+        {
+            if (studentId <= 0) {
+                return BadRequest();
+            }
+            var currStudent = StudentRepository.students.Where(s => s.Id == studentId).FirstOrDefault();
+            if (currStudent == null)
+            {
+                return NotFound();
+            }
+            StudentRepository.students.Remove(currStudent);
+            return NoContent();
+
+        }
+    }
+}
