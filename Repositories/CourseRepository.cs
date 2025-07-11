189e00541ed84558d4ca5ac88d807302cfaecc53
pbonala1 <pravalikabonala@gmail.com>
2025-07-11 15:45:53 -0500

    Created Repo clases and added details

---
+++ Repositories/CourseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FirstApp.Models;
+
+namespace FirstApp.Repositories
+{
+    public class CourseRepository
+    {
+        public CourseRepository()
+        {
+        }
+        List<Course> courses = new List<Course>
+        {
+            new Course{
+
+                CourseId=1,
+                CourseName="Computer Science"
+            },
+            new Course{
+
+                CourseId=2,
+                CourseName="IT"
+            }
+        };
+    
+    }
+}
+
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FirstApp.Models;
+
+namespace FirstApp.Repositories
+{
+	public class StudentRepository
+	{
+		public StudentRepository()
+		{
+		}
+
+		List<Student> students = new List<Student>
+		{
+			new Student{
+
+
+			Id = 1,
+			Name = "Sony",
+			Email = "sony@gmail.com",
+			EnrollmentDate = DateTime.Now
+				},
+			new Student
+			{
+			Id = 2,
+			Name = "Arun",
+			Email = "arun@gmail.com",
+			EnrollmentDate = DateTime.Now
+
+			}
+		};
+		
+
+
+    }
+}
+
