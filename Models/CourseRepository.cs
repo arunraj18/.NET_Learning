7cbcd77cd6f330553f476d61a13dd44a218548a8
apeddhala <arunraj.peddhala@dxc.com>
2025-07-11 15:26:42 -0500

    Created two repositories

---
+++ Models/CourseRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstApp.Models
+{
+    public static class CourseRepository
+    {
+        public static List<Course> {get; set;} = new List<Course>(){
+            new Course
+            {
+                CourseId = 1,
+                CourseName = "Maths",
+            }
+
+
+    }
+}
